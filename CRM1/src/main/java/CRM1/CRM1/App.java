bb8cd6a6959b865ad5748a7e18b952648fd9fb2b
Shreenibas500 <shreenibassamal500@gmail.com>
2025-03-12 16:38:37 +0530

    freamework added

---
+++ CRM1/src/main/java/CRM1/CRM1/App.java
@@ -0,0 +1,13 @@
+package CRM1.CRM1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
