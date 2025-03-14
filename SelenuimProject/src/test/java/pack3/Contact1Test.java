b50762db0776f841039021df8001e3af9117643c
Shreenibas500 <shreenibassamal500@gmail.com>
2025-03-14 15:16:19 +0530

    new framework added

---
+++ SelenuimProject/src/test/java/pack3/Contact1Test.java
@@ -0,0 +1,19 @@
+package pack3;
+
+import org.testng.annotations.Test;
+
+public class Contact1Test {
+	@Test
+	public void createcontactTest1() {
+		
+		System.out.println("execute createcontactTest1");
+	}
+	
+	@Test
+	public void modifycontactTest1() {
+		System.out.println("execute modifycontactTest1");
+	}
+
+
+
+}
---
+++ SelenuimProject/src/test/java/pack4/Contact2Test.java
@@ -0,0 +1,19 @@
+package pack4;
+
+import org.testng.annotations.Test;
+
+public class Contact2Test {
+	@Test
+	public void createcontactTest2() {
+		
+		System.out.println("execute createcontactTest2");
+	}
+	
+	@Test
+	public void modifycontactTest2() {
+		System.out.println("execute modifycontactTest2");
+	}
+
+
+
+}
